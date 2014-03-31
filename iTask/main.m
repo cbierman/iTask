7daeb384ce8414c3d29287b0bdbca4a58c6a08c7
Cale Bierman <cale-bierman@uiowa.edu>
2014-03-31 17:02:13 -0500

    Initial Commit

---
+++ iTask/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTask
+//
+//  Created by Cale Bierman on 3/31/14.
+//  Copyright (c) 2014 caleasmaadylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ITAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ITAppDelegate class]));
+    }
+}
